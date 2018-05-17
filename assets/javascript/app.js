ae98e49677a5cdda0c3f242719e52c415fb9711e
veena uppalapati <Veena@veenas-MacBook-Pro.local>
2018-05-17 15:29:39 -0400

    the full screen nav is hiding on the page load; on the hamburger trigger full screen nav is displayed; when close icon is triggered, full screen nav hides and shows pinkdiv and its relative elements

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
-    $('.pinkDiv').hide();
-    $('.shallHide').hide();
+    // on the page loads the full screen menu shall hide
+    $(".fullscreen_nav_menu").hide();
+
     $("#hamburger_icon").click(function(){
-        $(".text_wrapper").toggle();
+        $(".fullscreen_nav_menu").show();
+    });
+
+    $("#close_icon").click(function(){
+        $(".fullscreen_nav_menu").hide();
+        $('.shallHide').show();
+        $('.pinkDiv').show();      
     });
 });
