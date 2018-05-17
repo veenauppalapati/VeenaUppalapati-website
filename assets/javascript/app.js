6674bd4094498c018200b18ce8a181245a45a711
veena uppalapati <Veena@veenas-MacBook-Pro.local>
2018-05-17 16:13:38 -0400

    fixed the hamburger's position for both full screen and mobile

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     // on the page loads the full screen menu shall hide
     $(".fullscreen_nav_menu").hide();
-    $("#hamburger_icon_mobile").hide();
-    
+    // $("#hamburger_icon_mobile").hide();
+
     $("#hamburger_icon_fullscreen").click(function(){
         $(".fullscreen_nav_menu").show();
         $('.title_mobile').hide();
