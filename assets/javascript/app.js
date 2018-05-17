76435ddfc14ab57086d814ef74eab4da2bff4c07
veena uppalapati <Veena@veenas-MacBook-Pro.local>
2018-05-17 14:42:28 -0400

    added the text for the links and positioned it in the full screen neav menu

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-    
+    $('.pinkDiv').hide();
+    $('.shallHide').hide();
+    $("#hamburger_icon").click(function(){
+        $(".text_wrapper").toggle();
+    });
 });
