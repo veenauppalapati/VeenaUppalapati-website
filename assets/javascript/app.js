95fb7bc933b9e90530e5768a7899fcd60d73bdc0
veena uppalapati <Veena@veenas-MacBook-Pro.local>
2018-05-17 16:23:19 -0400

    fixed the bug with hamburger overlapping the close icon

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,19 @@
 $(document).ready(function(){
     // on the page loads the full screen menu shall hide
     $(".fullscreen_nav_menu").hide();
-    // $("#hamburger_icon_mobile").hide();
+
 
     $("#hamburger_icon_fullscreen").click(function(){
         $(".fullscreen_nav_menu").show();
         $('.title_mobile').hide();
+        $('.hamburger_icon_wrapper').hide();
     });
 
     $("#close_icon").click(function(){
         $(".fullscreen_nav_menu").hide();
         $('.shallHide').show();
-        $('.pinkDiv').show();      
+        $('.pinkDiv').show(); 
+        $('.hamburger_icon_wrapper').show();
+
     });
 });
