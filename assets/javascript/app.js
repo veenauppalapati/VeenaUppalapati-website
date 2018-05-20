5e8c57f224287938cda9528afd39f43a4145c4dd
veena uppalapati <Veena@veenas-MBP.fios-router.home>
2018-05-20 19:15:48 -0400

    changed the css color of the header in js and able to hide when carat is clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
     $(".fullscreen_nav_menu").hide();
 
+    $("#hamburger_icon_fullscreen").click(function(){
+        $(".fullscreen_nav_menu").show();
+       $('.overlay').hide();
+    });
+
+    $("#close_icon").click(function(){
+        $(".fullscreen_nav_menu").hide();
+       $('.overlay').show();
+    });
+
+    $('#caretDown_icon').click(function(){
+        $('.headerbg').addClass('headerColor');
+        $('h3, #hamburger_icon_fullscreen').css('color', 'white');
+        $('.hamburger_icon_wrapper').css('border-left', '1px solid white');
+        $('.portfolio_header').css('background-color', 'transparent');
+        $('.overlay').hide();
+    })
    
 });
