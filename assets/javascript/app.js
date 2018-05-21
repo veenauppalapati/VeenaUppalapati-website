5d043048dd8880136fd2d02140f412c4bda306b9
veena uppalapati <Veena@veenas-MBP.fios-router.home>
2018-05-21 15:09:05 -0400

    small icons are added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,27 @@
 $(document).ready(function(){
     $(".fullscreen_nav_menu").hide();
-
+$('#close_icon').hide();
+$('.portfolio').hide();
     $("#hamburger_icon_fullscreen").click(function(){
-        $(".fullscreen_nav_menu").show();
+        
+    //    $(this).removeClass('fa-align-justify').addClass('fa-window-close').attr('id', 'close_icon');
+       $(".fullscreen_nav_menu").show();
        $('.overlay').hide();
+
+       $('#close_icon').toggle();
+       $('#hamburger_icon_fullscreen').hide();
+       
     });
 
     $("#close_icon").click(function(){
+        console.log('hello');
         $(".fullscreen_nav_menu").hide();
        $('.overlay').show();
+
+     
+       $('#hamburger_icon_fullscreen').toggle();
+       $('#close_icon').hide();
+      
     });
 
     $('#caretDown_icon').click(function(){
@@ -17,6 +30,9 @@ $(document).ready(function(){
         $('.hamburger_icon_wrapper').css('border-left', '1px solid white');
         $('.portfolio_header').css('background-color', 'transparent');
         $('.overlay').hide();
-    })
+        $('.portfolio').toggle();
+        
+    });
+
    
 });
