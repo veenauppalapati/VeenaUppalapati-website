3f75bf101724c8926343c22720cb7fbc79822043
veena uppalapati <Veena@veenas-MBP.fios-router.home>
2018-05-29 00:23:57 -0400

    resume is changed to updated one

--- assets/javascript/contact.js
+++ assets/javascript/contact.js
@@ -8,12 +8,24 @@ $(document).ready(function(){
         var messageArea = $('#message-area').val();
         var messageDetails = `mailto:veena.uppalapati@nyfa.edu?subject=${subject}&body=${messageArea}%0A%0A- ${fullName}`
         console.log(messageDetails);
-
+        if( !$('input').val() ) {
+            $('#submit').hide(setTimeout(function(){
+                alert('please fill out the form')
+                $('#submit').show();
+            }, 1000));
+           
+        
+      }else{
+        $('#submit').show();
         $(this).attr('href', messageDetails);
         $('#fullname').val('');
         $('#subject').val('');
         $('#message-area').val('');
+      }
+       
     })
+
+   
 });
 // href=
 
